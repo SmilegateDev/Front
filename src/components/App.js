7f9e496f3b2a70916726ea74293bc834f39b4060
nova6296 <eppiko7@naver.com>
2020-03-08 21:29:45 +0900

    Auto commit

--- src/components/App.js
+++ src/components/App.js
@@ -23,9 +23,11 @@ class App extends Component {
       isLogin: false,
       location: null,
       replyContents: {},
+      feedReplyContents: {},
       year: null,
       month: null,
-      date: null
+      date: null,
+      isLastFeed: null
     }
   }
 
@@ -103,11 +105,12 @@ class App extends Component {
   handleItemClick = (e) => {
     const clicked = e.currentTarget.id;
 
-    if (clicked === "feed-on" && this.state.activeItem !== clicked) {   
+    if (clicked === "feed-on" && this.state.activeItem !== clicked) {
       const data = {
         year: this.state.year,
         month: this.state.month,
-        date: this.state.date
+        date: this.state.date,
+        isLastFeed: this.state.isLastFeed
       }
 
       const headers = {
@@ -119,15 +122,22 @@ class App extends Component {
   
       axios.post("/feed/getFeed", data, headers)
       .then(res => {
-        alert(res.data.isLastFeed);
+        const feedArr = res.data.Feed;
+        const likeArr = res.data.isLiked;
+
+        for (let i = 0; i < feedArr.length; i++) {
+          feedArr[i]['isLiked'] = likeArr[feedArr[i]._id];
+        }
 
         this.setState({
           year: res.data.Year,
           month: res.data.Month,
-          date: res.data.Date
+          date: res.data.Date,
+          isLastFeed: res.data.isLastFeed
         });
 
-        return this.setState({ feedData: res.data.Feed });
+        
+        return this.setState({ feedData: feedArr });
       })
       .catch(err => {
         alert(err);
@@ -171,6 +181,26 @@ class App extends Component {
       });
     }
 
+    if (clicked !== "feed-on") {
+      this.setState({
+        feedData: null,
+        feedReplyContents: {},
+        year: null,
+        month: null,
+        date: null,
+        isLastFeed: null
+      });
+    } else if (clicked === "feed-on" && this.state.activeItem === clicked) {
+      this.setState({
+        feedData: null,
+        feedReplyContents: {},
+        year: null,
+        month: null,
+        date: null,
+        isLastFeed: null
+      });
+    }
+
     if (window.markerLayer) {
       if (clicked !== "post-on") {
         this.setLocationNull();
@@ -190,6 +220,46 @@ class App extends Component {
         activeItem: clicked
       });
     }
+  }
+
+  getMoreFeedData = (e) => {
+    const data = {
+      year: this.state.year,
+      month: this.state.month,
+      date: this.state.date,
+      isLastFeed: this.state.isLastFeed
+    }
+
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("token")
+      }
+    };
+
+    axios.post("/feed/getFeed", data, headers)
+    .then(res => {
+      let exFeedData = this.state.feedData;
+      const feedArr = res.data.Feed;
+      const likeArr = res.data.isLiked;
+
+      for (let i = 0; i < feedArr.length; i++) {
+        feedArr[i]['isLiked'] = likeArr[feedArr[i]._id];
+        exFeedData.push(feedArr[i]);
+      }
+
+      this.setState({
+        year: res.data.Year,
+        month: res.data.Month,
+        date: res.data.Date,
+        isLastFeed: res.data.isLastFeed
+      });
+
+      return this.setState({ feedData: exFeedData });
+    })
+    .catch(err => {
+      alert(err);
+    });
   }
 
   toggleLike = (e) => {
@@ -329,12 +399,149 @@ class App extends Component {
     this.setState({postData: newPost });
   }
 
+  feedToggleLike = (e) => {
+    const data = {
+      objectId: e.currentTarget.dataset.id
+    }
+
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("token")
+      }
+    };
+
+    const index = e.currentTarget.dataset.index;
+
+    axios.post("/post/clickLike", data, headers)
+    .then(res => {
+      if (res.data.code === 200) {
+        let newFeed = this.state.feedData.slice();
+
+        if (res.data.message === "like") {
+          newFeed[index]['isLiked'] = 1;
+          newFeed[index]['likes_num']++;
+        } else if (res.data.message === "unlike") {
+          newFeed[index]['isLiked'] = 0;
+          newFeed[index]['likes_num']--;
+        }
+
+        this.setState({ feedData: newFeed });
+      } else {
+        alert(res.data.message);
+      }
+    })
+    .catch(err => {
+      alert(err);
+    });
+  }
+
+  feedHandleReplyChange = (e) => {
+    let newReply = this.state.feedReplyContents;
+
+    newReply[e.currentTarget.dataset.id] = e.currentTarget.value;
+
+    this.setState({feedReplyContents: newReply});
+  }
+
+  feedHandleReply = (e) => {
+    if (this.state.feedReplyContents[e.currentTarget.dataset.id] === undefined || this.state.feedReplyContents[e.currentTarget.dataset.id].length === 0) {
+      return alert("댓글을 입력해주세요");
+    }
+
+    const data = {
+      objectId: e.currentTarget.dataset.id,
+      replyContents: this.state.feedReplyContents[e.currentTarget.dataset.id]
+    }
+
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("token")
+      }
+    };
+
+    const id = e.currentTarget.dataset.id;
+    const index = e.currentTarget.dataset.index;
+
+    axios.post("/post/createReply", data, headers)
+    .then(res => {
+      if (res.data.code === 200) {
+        if (this.state.feedData[index]['replyWriter']) {
+          let newFeed = this.state.feedData.slice();
+
+          newFeed[index]['replyWriter'].push(jwt.decode(localStorage.getItem("token")).nickname);
+          newFeed[index]['replyContents'].push(this.state.feedReplyContents[id]);
+          newFeed[index]['reply_num']++;
+
+          this.setState({ feedData: newFeed });
+        } else {
+          let newFeed = this.state.feedData.slice();
+
+          newFeed[index]['reply_num']++;
+
+          this.setState({ feedData: newFeed });
+        }
+      } else {
+        alert(res.data.message);
+      }
+    })
+    .catch(err => {
+      alert(err);
+    });
+  }
+
+  feedHandleGetReply = (e) => {
+    const data = {
+      objectId: e.currentTarget.dataset.id
+    }
+
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("token")
+      }
+    };
+
+    const index = e.currentTarget.dataset.index;
+
+    axios.post("/post/getReply", data, headers)
+    .then(res => {
+      let newFeed = this.state.feedData.slice();
+
+      const writerArr = Object.keys(res.data).map(function(key) {
+        return [res.data[key].writer];
+      });
+
+      const contentsArr = Object.keys(res.data).map(function(key) {
+        return [res.data[key].replyContents];
+      });
+
+      newFeed[index]['replyWriter'] = writerArr;
+      newFeed[index]['replyContents'] = contentsArr;
+
+      this.setState({ feedData: newFeed });
+    })
+    .catch(err => {
+      alert("오류가 발생했습니다.");
+    });
+  }
+
+  feedHandleFoldReply = (e) => {
+    let newFeed = this.state.feedData.slice();
+
+    newFeed[e.currentTarget.dataset.index]['replyWriter'] = null;
+    newFeed[e.currentTarget.dataset.index]['replyContents'] = null;
+
+    this.setState({ feedData: newFeed });
+  }
+
   render() {
     return (
       <Fragment>
         <div id="vmap" />
         <Sidebar handleItemClick={this.handleItemClick} isLogin={this.state.isLogin} setLoginState={this.setLoginState} setActiveItem={this.setActiveItem} setAllDataNull={this.setAllDataNull}/>
-        <Feed activeItem={this.state.activeItem} feedData={this.state.feedData} />
+        <Feed activeItem={this.state.activeItem} feedData={this.state.feedData} toggleLike={this.feedToggleLike} handleReplyChange={this.feedHandleReplyChange} handleReply={this.feedHandleReply} handleGetReply={this.feedHandleGetReply} handleFoldReply={this.feedHandleFoldReply} isLastFeed={this.state.isLastFeed} getMoreFeedData={this.getMoreFeedData} />
         <Login activeItem={this.state.activeItem} setLoginState={this.setLoginState} setActiveItem={this.setActiveItem}/>
         <Join activeItem={this.state.activeItem} setActiveItem={this.setActiveItem} />
         <Post activeItem={this.state.activeItem} location={this.state.location} setLocationNull={this.setLocationNull}/>
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Feed extends Component {
   constructor(props) {
@@ -11,13 +11,51 @@ class Feed extends Component {
         <div className="head custom-head">피드</div><hr className="feed-hr" />
         <article className="custom-article">
           <div className="mainarticle">
-            {this.props.feedData ? this.props.feedData.map(feed => {
-              return <div className="text-left mt-3"><h2>{ feed.title }</h2><div><img src="default.jpg" className="img-fluid my-4" alt="default"/></div><div>{ feed.contents }</div><hr className="feed-hr my-4" /></div>
-            }) : '데이터 불러오는 중'}
+            {this.props.feedData ? this.props.feedData.map((feed, index) => {
+              return <div className="text-left mt-3">
+                <h2>{ feed.title }</h2>
+                <div><img src="default.jpg" className="img-fluid my-4" alt="default"/></div>
+                <div className="mb-4">
+                  {feed.isLiked === 0 ?
+                    <button className="btn btn-success user-profile-btn" onClick={this.props.toggleLike} data-id={feed._id} data-index={index}>좋아요</button> :
+                    <button className="btn btn-danger user-profile-btn" onClick={this.props.toggleLike} data-id={feed._id} data-index={index}>좋아요 취소</button>
+                  }
+                </div>
+                <div>{ feed.likes_num }명이 좋아합니다.</div>
+                <div className="mt-2">{feed.writer}) { feed.contents }</div>
+                { feed.replyWriter ?
+                  <Fragment>
+                    <div className="mt-4 get-reply mb-3" onClick={this.props.handleFoldReply} data-index={index}>댓글 접기</div>
+                      {feed.replyWriter.map((writer, index) => {
+                        return <div><span className="custom-user-list" onClick={this.handleUserPost}>{writer}</span>) {feed.replyContents[index]}</div>
+                      })}
+                  </Fragment> :
+                  <div className="mt-4 get-reply" data-id={feed._id} onClick={this.props.handleGetReply} data-index={index}>댓글 { feed.reply_num }개 모두 보기</div>
+                }
+                <div className="mt-3">
+                  <form method="POST" onSubmit={e => { e.preventDefault(); }}>
+                    <div className="form-group">
+                      <div className="input-group">
+                        <input type="text" className="form-control custom-reply-form" name="reply" id="reply" placeholder="댓글 달기..." data-id={feed._id} onChange={this.props.handleReplyChange} />
+                        <div className="input-group-append">
+                          <span className="input-group-text custom-reply-btn"><i class="far fa-comment-dots" data-id={feed._id} data-index={index} onClick={this.props.handleReply}></i>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <hr className="feed-hr my-4" />
+            </div>
+            }) : null}
+            { this.props.isLastFeed === 1 ?
+              <div className="mb-5">마지막 글입니다.</div> :
+              <button className="btn btn-primary btn-block user-profile-btn mb-5" onClick={this.props.getMoreFeedData}>더 보기</button>
+            }
           </div>
         </article>
       </div>
-   );
+    );
   }
 }
 
